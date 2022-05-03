f427097c1b237caa0f48a014835a2b2b1b719c91
lucasplr <lucasriffel55@gmail.com>
2022-05-03 16:02:33 -0300

    update

--- memory/app.js
+++ memory/app.js
@@ -143,3 +143,14 @@ boardCreator()
 
 
 
+const nav = document.querySelector(".nav")
+const btn = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+
+btn.addEventListener("click", () => {
+    if(nav.classList.contains("nav-show")){
+        nav.classList.remove("nav-show")
+    }else{
+        nav.classList.add("nav-show")
+    }
+})
